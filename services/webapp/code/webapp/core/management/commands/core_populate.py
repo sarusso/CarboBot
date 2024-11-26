1a4f0b8e2e2eb680c900ef329a062be560380a3d
Stefano Alberto Russo <stefano.russo@gmail.com>
2024-11-27 00:40:26 +0100

    webapp: added demo foods and observations in the pouplate

--- services/webapp/code/webapp/core/management/commands/core_populate.py
+++ services/webapp/code/webapp/core/management/commands/core_populate.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-# from ...models import Profile, Container, Computing, Storage, KeyPair, Page
-#
+from ...models import Food, FoodObservation
+
 class Command(BaseCommand):
     help = 'Adds the admin superuser with \'a\' password.'
 
@@ -23,3 +23,42 @@ class Command(BaseCommand):
             testuser.is_admin=True
             testuser.is_superuser=True
             testuser.save() 
+
+
+        #=====================
+        #  Demo Food
+        #=====================
+        foods = Food.objects.all()
+        if foods:
+            print('Not creating demo foods as they already exists')
+        else:
+            demo_foods = [
+                { "uuid": "00000000-0000-0000-0000-000000000001", "cho_ratio": 0.01, "name": "Insalata caprese", "main_ingredients": ["mozzarella", "pomodoro", "basilico"]},
+                { "uuid": "00000000-0000-0000-0000-000000000002", "cho_ratio": 0.35, "name": "Insalata di riso con verdure", "main_ingredients": ["riso", "carote", "piselli"]},
+                { "uuid": "00000000-0000-0000-0000-000000000003", "cho_ratio": 0.35, "name": "Insalata di riso con pollo", "main_ingredients": ["riso", "pollo", "maionese"]},
+                { "uuid": "00000000-0000-0000-0000-000000000004", "cho_ratio": 0.35, "name": "Insalata di riso con sottaceti vari", "main_ingredients": ["riso", "sottaceti", "olio"]},
+                { "uuid": "00000000-0000-0000-0000-000000000038", "cho_ratio": 0.55, "name": "Arancini di riso", "main_ingredients": ["riso", "carne", "piselli"]}]
+            for demo_food in demo_foods:
+                food = Food(uuid = demo_food['uuid'],
+                            name = demo_food['name'],
+                            main_ingredients = demo_food['main_ingredients'],
+                            created_by = testuser)
+                food.save()
+
+                # Create also some demo observations
+                FoodObservation.objects.create(food = food,
+                                               cho_ratio = demo_food['cho_ratio'],
+                                               created_by = testuser)
+
+            print('Added demo foods')
+
+
+
+
+
+
+
+
+
+
+
