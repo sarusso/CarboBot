367b578b396e3e23804519f1306fe037f160009f
Stefano Alberto Russo <stefano.russo@gmail.com>
2025-01-09 00:28:21 +0100

    elastic: improved initialization and fixed bug

--- services/elastic/initialize.sh
+++ services/elastic/initialize.sh
@@ -2,7 +2,7 @@
 
 # Wait for Elasticsearch to start
 until curl -s http://localhost:9200 >/dev/null; do
-    echo "Waiting for Elasticsearch to start..."
+    echo "[INIT] Waiting for Elasticsearch to start..."
     sleep 2
 done
 
@@ -11,7 +11,7 @@ INDEX_NAME="food_index"
 
 # Check if the index exists
 if ! curl -s -o /dev/null -w "%{http_code}" http://localhost:9200/$INDEX_NAME | grep -q "200"; then
-    echo "Index $INDEX_NAME does not exist. Creating..."
+    echo "[INIT] Index $INDEX_NAME does not exist. Creating..."
 
     # Create the index
     curl -X PUT http://localhost:9200/$INDEX_NAME -H 'Content-Type: application/json' -d '{
@@ -24,19 +24,19 @@ if ! curl -s -o /dev/null -w "%{http_code}" http://localhost:9200/$INDEX_NAME | 
           }
         }
         '
-    echo "Created."
+    echo "[INIT] Created."
 else
-    echo "Index $INDEX_NAME already exists."
+    echo "[INIT] Index $INDEX_NAME already exists."
 fi
 
 # Define the test index name
 INDEX_NAME="food_index_test"
 
-# Check if the index exists
+# Check if the test index exists
 if ! curl -s -o /dev/null -w "%{http_code}" http://localhost:9200/$INDEX_NAME | grep -q "200"; then
-    echo "Index $INDEX_NAME does not exist. Creating..."
+    echo "[INIT] Index $INDEX_NAME does not exist. Creating..."
 
-    # Create the index
+    # Create the test index
     curl -X PUT http://localhost:9200/$INDEX_NAME -H 'Content-Type: application/json' -d '{
           "mappings": {
             "properties": {
@@ -47,10 +47,9 @@ if ! curl -s -o /dev/null -w "%{http_code}" http://localhost:9200/$INDEX_NAME | 
           }
         }
         '
-    echo "Created."
+    echo "[INIT] Created."
 else
-    echo "Index $INDEX_NAME already exists."
+    echo "[INIT] Index $INDEX_NAME already exists."
 fi
 
-# Start Elasticsearch
-exec /usr/local/bin/docker-entrypoint.sh eswrapper
+echo "[INIT] All done."
